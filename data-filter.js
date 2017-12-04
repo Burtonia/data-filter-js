6cfbe85d08c6732857c00d5c902e00f9948245c7
Alexander Burton <hi@alexburton.com>
2017-12-03 22:48:38 -0500

    cleaned up each loops and fixed all button resetter.

--- data-filter.js
+++ data-filter.js
@@ -1,8 +1,8 @@
-// Data Filter JS v1.0.2 (2017-11-07)
+// Data Filter JS v1.0.3 (2017-12-04)
 // Simple and Flexible Data Filter for Click and Keystroke Events
 // The MIT License (MIT)
 // Copyright (c) 2017 Alexander Burton
-// Last Change: [2017-11-07]
+// Last Change: [2017-12-04]
 // Uppercase method added to node and search_string to eliminate case sensitivity on keystroke.
 // Case sensitivity will be removed from button search next.
 // Masonry Optional with a simple boolean in the config object.
@@ -28,18 +28,20 @@ function DataFilterJS(config){
     var eventFilter = e.currentTarget.attributes[1].nodeValue;
      // Loop through Filter Nodes on Button Click
      $.each(filter_node, function(i, v) {
-        if(eventFilter == "show-all") {
+        if(eventFilter == "*") {
           filter_node[i].classList.remove('filter-me');
         } else if(filter_node[i].getAttribute('data-filter').includes(eventFilter) == false) {
           filter_node[i].classList.add('filter-me')
         } else {filter_node[i].classList.remove('filter-me');}
      });
+     // Masonry JS Scripts
+     initMasonry();
   }); // end click event
 
   // (Keystroke) Data Filter Function
   $(filterButtonGroup+' > input[type="text"]').on('keypress keydown keyup', function(e, fnd) {
     // Loop through Filter Nodes on Keystroke
-    $.each($(filterNode), function(i, v) {
+    $.each(filter_node, function(i, v) {
       var node = v.getAttribute("data-filter").toString().toUpperCase();
       var search_string = $(filterButtonGroup+' > '+'input[type="text"]').val().toUpperCase();
       //console.log(node.getAttribute('data-filter'));
@@ -50,17 +52,22 @@ function DataFilterJS(config){
       }
     }); // end each
     // Masonry JS Scripts
+    initMasonry();
+  }); // end keypress listener
+
+  // Init Masonry Function
+  function initMasonry() {
     if(config.masonry == true) {
       $(filterGroup).masonry({itemSelector: filterNode+':not(.filter-me)', columnWidth: config.columnWidth});
     }
-  }); // end keypress listener
+  }
 }
 
-var example = new DataFilterJS({
-  filterGroup: '.grid-selector',
-  filterNode: '.node-selector',
-  filterButtonGroup: '.filter-button-group',
-  transitionSpeed: '0.5s',
-  masonry: true,
-  columnWidth: 150
-});
+// var example = new DataFilterJS({
+//   filterGroup: '.grid-selector',
+//   filterNode: '.node-selector',
+//   filterButtonGroup: '.filter-button-group',
+//   transitionSpeed: '0.5s',
+//   masonry: true,
+//   columnWidth: 150
+// });
